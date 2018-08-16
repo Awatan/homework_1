71819d4814ae3a8716f435025696430bfbe8392f
Natali <natelier.mai@gmail.com>
2018-08-16 23:16:39 +0300

    Removed comments

--- homework1.py
+++ homework1.py
@@ -1,34 +1,35 @@
-# 1
-#a = int(input('Введите число a: '))
-#b = int(input('Введите число b: '))
-#f = int(input('Введите число f: '))
-#result = ((a + b - f / a) + f*a*a - (a+b))
-#print('Результат: ', result)
+#1
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+f = int(input('Введите число f: '))
+result = ((a + b - f / a) + f*a*a - (a+b))
+print('Результат: ', result)
 
 #2
-#year_born = int(input('Please, enter the year when you was born: '))
-#years_old = 2018 - year_born
-#print('You are', years_old, 'old')
+year_born = int(input('Please, enter the year when you was born: '))
+years_old = 2018 - year_born
+print('You are', years_old, 'old')
 
 #3
-#meters = int(input('Enter m: '))
-#kilometers = meters / 1000
-#print(kilometers, 'kilometers')
+meters = int(input('Enter m: '))
+kilometers = meters / 1000
+print(kilometers, 'kilometers')
 
 #4 варіант, що відрізняється від того, що розбирали
-#name = input('Please, enter your name: ')
-#line_1 = int(len(name))
-#space_1 = int((18 - line_1))
-#space_2 = int((18 - line_1))
-#if space_1 % 2 == 0:
-#   space_1 = space_1
-#else:
-#   space_1 = space_1 + 1
-#print('x' * 20)
-#print('x' + ' ' * (space_1 // 2) + name + ' ' * (space_2 // 2) + 'x')
-#print('x' * 20)
+name = input('Please, enter your name: ')
+line_1 = int(len(name))
+space_1 = int((18 - line_1))
+space_2 = int((18 - line_1))
+if space_1 % 2 == 0:
+  space_1 = space_1
+else:
+  space_1 = space_1 + 1
+print('x' * 20)
+print('x' + ' ' * (space_1 // 2) + name + ' ' * (space_2 // 2) + 'x')
+print('x' * 20)
 
-#n = int(input('How many minutes pass?: '))
-#hours = n // 60 % 24
-#minutes = n % 60
-#print(hours, ':', minutes)
+#5
+n = int(input('How many minutes pass?: '))
+hours = n // 60 % 24
+minutes = n % 60
+print(hours, ':', minutes)
